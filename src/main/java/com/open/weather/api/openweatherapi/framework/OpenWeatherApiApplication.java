1d11f8cc2b6dc25bff1018033830b6566bd7f0da
CemAkr30 <cemakar18@gmail.com>
2025-05-16 17:53:22 +0300

    observerbility & helm charts & nexus & pipeline stages

--- src/main/java/com/open/weather/api/openweatherapi/framework/OpenWeatherApiApplication.java
+++ src/main/java/com/open/weather/api/openweatherapi/framework/OpenWeatherApiApplication.java
@@ -1,11 +1,13 @@
-package com.open.weather.api.openweatherapi;
+package com.open.weather.api.openweatherapi.framework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.open.weather.api.openweatherapi.*"
+})
 @EnableCaching
 @EnableScheduling
 public class OpenWeatherApiApplication {
